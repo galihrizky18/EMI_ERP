d1f7959f096a133586479e814851595b9fe6b995
galihrizky18 <galihrizkycode@gmail.com>
2024-11-26 08:46:57 +0700

    instal xunit.runner

---
+++ XUnit/Tes1.vb
@@ -0,0 +1,30 @@
+﻿Imports Xunit
+
+Public Class Tes1
+
+    Private ReadOnly TesUnit As New TesUnit
+
+
+    <Fact>
+    Public Sub Tes1()
+
+        'arrange (Initial Data)
+        Dim a As Integer = 2
+        Dim b As Integer = 3
+
+        'act (Action)
+        Dim result As String = TesUnit.Tambah(a, b)
+
+        'assert
+        Assert.Equal("5", result)
+
+    End Sub
+
+
+
+
+
+
+
+
+End Class
---
+++ XUnit/TesUnit.vb
@@ -0,0 +1,17 @@
+﻿Public Class TesUnit
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Label1.Text = Tambah(2, 3)
+        MessageBox.Show(Tambah(2, 3))
+
+
+    End Sub
+
+
+    Public Function Tambah(a, b) As String
+        Return a + b
+    End Function
+
+
+End Class
